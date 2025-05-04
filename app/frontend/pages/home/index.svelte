f1da1349b813e2a1c5a8e9b7b17b510c6df3a3e2
Diego Marrufo <yojouhanshumi@gmail.com>
2025-05-05 00:43:08 +0200

    remove unused component

--- app/frontend/pages/home/index.svelte
+++ app/frontend/pages/home/index.svelte
@@ -1,19 +1,7 @@
 <script>
-  import { Progress } from "melt/components";
-
   import Layout from "../../components/Layout.svelte";
 </script>
 
 <Layout>
   <h1>IS it OI</h1>
-  <Progress>
-    {#snippet children(progress)}
-      <div {...progress.root}>
-        <div
-          {...progress.progress}
-          data-melt-progress-progress={`${progress.progress}`}
-        ></div>
-      </div>
-    {/snippet}
-  </Progress>
 </Layout>
